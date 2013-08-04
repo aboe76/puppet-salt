3ac6e6de6f618bffe453cf25f756d0a487f039db
Niels Abspoel <aboe76@gmail.com>
2013-08-04 20:58:08 +0200

    updated spec file to reflect this

--- spec/classes/salt_spec.rb
+++ spec/classes/salt_spec.rb
@@ -10,7 +10,7 @@ describe 'salt' do
     end
   end
 
-  ['Debian', 'RedHat', 'SUSE', 'Archlinux'].each do |distro|
+  ['Debian', 'RedHat', 'SUSE', ].each do |distro|
     context "on #{distro}" do
       let(:facts) {{
           :osfamily => distro,
@@ -22,20 +22,20 @@ describe 'salt' do
       it { should contain_class('salt::minion::install') }
       it { should contain_class('salt::minion::config') }
       it { should contain_class('salt::minion::service') }
-        
+
       ##
       ## salt-master config file
       ##
       describe 'config file with default params' do
         it { should contain_file('/etc/salt/master')}
       end
-      
+
       ##
       ## salt-minion config file
       ##
       describe 'config file with default params' do
         it { should contain_file('/etc/salt/minion')}
-      end  
+      end
 
       ##
       ## salt-master service
@@ -76,6 +76,76 @@ describe 'salt' do
         should contain_package('salt-minion').with(
         'ensure'   => 'present',
         'name'     => 'salt-minion'
+        )
+      end
+    end
+  end
+  ['Archlinux', ].each do |distro|
+    context "on #{distro}" do
+      let(:facts) {{
+          :osfamily => distro,
+        }}
+
+      it { should contain_class('salt::master::install') }
+      it { should contain_class('salt::master::config') }
+      it { should contain_class('salt::master::service') }
+      it { should contain_class('salt::minion::install') }
+      it { should contain_class('salt::minion::config') }
+      it { should contain_class('salt::minion::service') }
+
+      ##
+      ## salt-master config file
+      ##
+      describe 'config file with default params' do
+        it { should contain_file('/etc/salt/master')}
+      end
+
+      ##
+      ## salt-minion config file
+      ##
+      describe 'config file with default params' do
+        it { should contain_file('/etc/salt/minion')}
+      end
+
+      ##
+      ## salt-master service
+      ##
+      describe 'service with default params' do
+        it { should contain_service('salt-master').with(
+          'ensure'     => 'running',
+          'enable'     => 'true',
+          'hasstatus'  => 'true',
+          'hasrestart' => 'true'
+          )}
+      end
+      ##
+      ## salt-minion service
+      ##
+      describe 'service with default params' do
+        it { should contain_service('salt-minion').with(
+          'ensure'     => 'running',
+          'enable'     => 'true',
+          'hasstatus'  => 'true',
+          'hasrestart' => 'true'
+          )}
+      end
+
+      ##
+      ## salt::master::install
+      ##
+      it 'installs the salt-master package' do
+        should contain_package('salt').with(
+        'ensure'   => 'present',
+        'name'     => 'salt'
+        )
+      end
+      ##
+      ## salt::minion::install
+      ##
+      it 'installs the salt-minion package' do
+        should contain_package('salt').with(
+        'ensure'   => 'present',
+        'name'     => 'salt'
         )
       end
     end
