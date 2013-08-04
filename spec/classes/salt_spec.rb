303136f83aed1397db33ac178bb1fc134bb2993b
Niels Abspoel <aboe76@gmail.com>
2013-08-04 19:25:37 +0200

    update salt_spec.rb with better style

--- spec/classes/salt_spec.rb
+++ spec/classes/salt_spec.rb
@@ -1,91 +1,65 @@
-require 'spec_helper' 
+require 'spec_helper'
 
-describe 'salt', :type => 'class' do 
-  context "On a Debian OS with no package name specified" do 
-    let :facts do 
-      { 
-        :osfamily => 'Debian' 
-      } 
-    end 
-    
-    it { 
-      should contain_package('salt').with( { 'name' => 'salt' } ) 
+describe 'salt' do
+
+  context 'on unsupported distributions' do
+    let(:facts) {{ :osfamily => 'Unsupported' }}
+
+    it 'we fail' do
+      expect { subject }.to raise_error(/not supported on an Unsupported/)
+    end
+  end
+
+  context "On a Debian OS with no package name specified" do
+    let(:facts) {{ :osfamily => 'Debian' }}
+    it 'includes salt::master' do
+      should contain_package('salt').with( { 'name' => 'salt' } )
       should contain_package('salt-master').with( { 'name' => 'salt-master' } )
+      should contain_service('salt-master').with( { 'name' => 'salt-master' } )
+    end
+    it 'includes salt::minion' do
       should contain_package('salt-minion').with( { 'name' => 'salt-minion' } )
-      should contain_service('salt-master').with( { 'name' => 'salt-master' } ) 
       should contain_service('salt-minion').with( { 'name' => 'salt-minion' } )
-    } 
+    end
   end
-  
-  context "On a RedHat OS with no package name specified" do 
-    let :facts do 
-      { 
-        :osfamily => 'RedHat' 
-      } 
-    end 
-   
-    it { 
-      should contain_package('salt').with( { 'name' => 'salt' } ) 
+
+  context "On a RedHat OS with no package name specified" do
+    let(:facts) {{ :osfamily => 'Redhat' }}
+    it 'includes salt::master' do
+      should contain_package('salt').with( { 'name' => 'salt' } )
       should contain_package('salt-master').with( { 'name' => 'salt-master' } )
+      should contain_service('salt-master').with( { 'name' => 'salt-master' } )
+    end
+    it 'includes salt::minion' do
       should contain_package('salt-minion').with( { 'name' => 'salt-minion' } )
-      should contain_service('salt-master').with( { 'name' => 'salt-master' } ) 
-      should contain_service('salt-minion').with( { 'name' => 'salt-minion' } ) 
-    } 
+      should contain_service('salt-minion').with( { 'name' => 'salt-minion' } )
+    end
   end
-  
-  context "On a Archlinux system with no package name specified" do 
-    let :facts do 
-      { 
-        :osfamily => 'Archlinux' 
-      } 
-    end 
-   
-    it { 
-      should contain_package('salt').with( { 'name' => 'salt' } ) 
+
+  context "On a Archlinux system with no package name specified" do
+    let(:facts) {{ :osfamily => 'Archlinux' }}
+    it 'includes salt::master' do
+      should contain_package('salt').with( { 'name' => 'salt' } )
       should contain_package('salt-master').with( { 'name' => 'salt-master' } )
+      should contain_service('salt-master').with( { 'name' => 'salt-master' } )
+    end
+    it 'includes salt::minion' do
       should contain_package('salt-minion').with( { 'name' => 'salt-minion' } )
-      should contain_service('salt-master').with( { 'name' => 'salt-master' } ) 
-      should contain_service('salt-minion').with( { 'name' => 'salt-minion' } ) 
-    } 
+      should contain_service('salt-minion').with( { 'name' => 'salt-minion' } )
+    end
   end
-  
-  context "On a Suse OS with no package name specified" do 
-    let :facts do 
-      { 
-        :osfamily => 'Suse' 
-      } 
-    end 
-   
-    it { 
-      should contain_package('salt').with( { 'name' => 'salt' } ) 
+
+  context "On a Suse OS with no package name specified" do
+    let(:facts) {{ :osfamily => 'Suse' }}
+    it 'includes salt::master' do
+      should contain_package('salt').with( { 'name' => 'salt' } )
       should contain_package('salt-master').with( { 'name' => 'salt-master' } )
+      should contain_service('salt-master').with( { 'name' => 'salt-master' } )
+    end
+    it 'includes salt::minion' do
       should contain_package('salt-minion').with( { 'name' => 'salt-minion' } )
-      should contain_service('salt-master').with( { 'name' => 'salt-master' } ) 
-      should contain_service('salt-minion').with( { 'name' => 'salt-minion' } ) 
-    } 
-  end 
-  
-  context "On an unknown OS with no package name specified" do 
-    let :facts do 
-      { 
-        :osfamily => 'Darwin' 
-      } 
-    end 
-    it {
-      expect { should raise_error(Puppet::Error) } 
-    } 
-  end 
-  
-  context "With a package name specified" do 
-    let :params do 
-      { 
-        :package_name => 'salt-git' 
-      } 
-    end 
-    
-    it { 
-      should contain_package('salt-git').with( { 'name' => 'salt-git' } ) 
-      should contain_service('salt-master').with( { 'name' => 'salt-master' } ) 
-    } 
-  end 
+      should contain_service('salt-minion').with( { 'name' => 'salt-minion' } )
+    end
+  end
+
 end   
