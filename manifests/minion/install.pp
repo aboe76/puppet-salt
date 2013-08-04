21351782f627d914c94502d7f0e962a65059dac6
Niels Abspoel <aboe76@gmail.com>
2013-08-04 21:19:02 +0200

    fix style issues

--- manifests/minion/install.pp
+++ manifests/minion/install.pp
@@ -2,8 +2,8 @@ class salt::minion::install (
   $minion_package_ensure = $salt::minion::minion_package_ensure,
   $minion_package_name   = $salt::minion::minion_package_name,) inherits
 salt::minion {
-  if defined(Package["$salt::minion::minion_package_name"]) == false {
-    package { "$salt::minion::minion_package_name": ensure =>
+  if defined(Package["${salt::minion::minion_package_name}"]) == false {
+    package { "${salt::minion::minion_package_name}": ensure =>
       $salt::minion::minion_package_ensure, }
 
   }
