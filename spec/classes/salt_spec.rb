17dbe76a01a27e84c84bcc2ce74429900ef1f286
Niels Abspoel <aboe76@gmail.com>
2013-08-04 19:40:27 +0200

    update

--- spec/classes/salt_spec.rb
+++ spec/classes/salt_spec.rb
@@ -10,107 +10,19 @@ describe 'salt' do
     end
   end
 
-  context "On a Debian OS with no package name specified" do
-    let(:facts) {{ :osfamily => 'Debian' }}
-    it 'includes salt::master' do
-      it 'includes salt::master::install' do
-        should contain_package('salt').with( { 'name' => 'salt' } )
+  ['Debian', 'RedHat', 'SUSE', 'Archlinux'].each do |distro|
+    context "on #{distro}" do
+      let(:facts) {{
+        :osfamily => distro,
+      }}
+      
+      it { should contain_class('salt::master::install') }
+      it { should contain_class('salt::master::config') }
+      it { should contain_class('salt::master::service') }
+      it { should contain_class('salt::minion::install') }
+      it { should contain_class('salt::minion::config') }
+      it { should contain_class('salt::minion::service') }
       end
-      it 'includes salt::master::config' do
-        should contain_file('/etc/salt/master')
-      end
-      it 'includes salt::master::service' do
-        should contain_service('salt-master').with( { 'name' => 'salt-master' } )
-        end
-    end
-    it 'includes salt::minion' do
-      it 'includes salt::minion::install' do
-        should contain_package('salt-minion').with( { 'name' => 'salt-minion' } )
-      end
-      it 'includes salt::minion::config' do
-        should contain_file('/etc/salt/minion')
-      end
-      it 'includes salt::minion::service' do
-        should contain_service('salt-minion').with( { 'name' => 'salt-minion' } )
-      end
-    end
-  end
-
-  context "On a RedHat OS with no package name specified" do
-    let(:facts) {{ :osfamily => 'Redhat' }}
-    it 'includes salt::master' do
-      it 'includes salt::master::install' do
-        should contain_package('salt').with( { 'name' => 'salt' } )
-      end
-      it 'includes salt::master::config' do
-        should contain_file('/etc/salt/master')
-      end
-      it 'includes salt::master::service' do
-        should contain_service('salt-master').with( { 'name' => 'salt-master' } )
-        end
-    end
-    it 'includes salt::minion' do
-      it 'includes salt::minion::install' do
-        should contain_package('salt-minion').with( { 'name' => 'salt-minion' } )
-      end
-      it 'includes salt::minion::config' do
-        should contain_file('/etc/salt/minion')
-      end
-      it 'includes salt::minion::service' do
-        should contain_service('salt-minion').with( { 'name' => 'salt-minion' } )
-      end
-    end
-  end
-  
-  context "On a Archlinux system with no package name specified" do
-    let(:facts) {{ :osfamily => 'Archlinux' }}
-    it 'includes salt::master' do
-      it 'includes salt::master::install' do
-        should contain_package('salt').with( { 'name' => 'salt' } )
-      end
-      it 'includes salt::master::config' do
-        should contain_file('/etc/salt/master')
-      end
-      it 'includes salt::master::service' do
-        should contain_service('salt-master').with( { 'name' => 'salt-master' } )
-        end
-    end
-    it 'includes salt::minion' do
-      it 'includes salt::minion::install' do
-        should contain_package('salt-minion').with( { 'name' => 'salt-minion' } )
-      end
-      it 'includes salt::minion::config' do
-        should contain_file('/etc/salt/minion')
-      end
-      it 'includes salt::minion::service' do
-        should contain_service('salt-minion').with( { 'name' => 'salt-minion' } )
-      end
-    end
-  end
-  
-  context "On a Suse OS with no package name specified" do
-    let(:facts) {{ :osfamily => 'Suse' }}
-    it 'includes salt::master' do
-      it 'includes salt::master::install' do
-        should contain_package('salt').with( { 'name' => 'salt' } )
-      end
-      it 'includes salt::master::config' do
-        should contain_file('/etc/salt/master')
-      end
-      it 'includes salt::master::service' do
-        should contain_service('salt-master').with( { 'name' => 'salt-master' } )
-        end
-    end
-    it 'includes salt::minion' do
-      it 'includes salt::minion::install' do
-        should contain_package('salt-minion').with( { 'name' => 'salt-minion' } )
-      end
-      it 'includes salt::minion::config' do
-        should contain_file('/etc/salt/minion')
-      end
-      it 'includes salt::minion::service' do
-        should contain_service('salt-minion').with( { 'name' => 'salt-minion' } )
-      end
-    end
   end
 end
+      
